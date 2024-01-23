d89fab9cac5ddf4e80cdfd272a5812e6bb21de97
Mike Bennett <mbennett@datacite.org>
2024-01-23 21:15:32 +0000

    Add DataFileStatus enum (#2)

--- app/enums.py
+++ app/enums.py
@@ -16,3 +16,11 @@ class UserLevel(Enum):
     editor = 2  # Can create and edit data files
     staff = 3  # Can edit users and permissions
     admin = 4  # Can do everything
+
+
+class DataFileStatus(Enum):
+    """The status of a data file."""
+    active = "Available"
+    generating = "Generating"
+    hidden = "Hidden"
+    deleted = "Deleted"
