322a6867c92a11acc4a91dd28522df6fd7df2c18
Mike Bennett <mbennett@datacite.org>
2024-01-23 23:42:14 +0000

    Add user password functionality

--- app/models/users.py
+++ app/models/users.py
@@ -1,6 +1,9 @@
 from ..dependencies.database import Base
 from ..enums import UserLevel
 from sqlalchemy import Column, Integer, String, Boolean, Enum
+from passlib.context import CryptContext
+
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class User(Base):
@@ -11,3 +14,10 @@ class User(Base):
     password = Column(String, nullable=False)
     user_level = Column(Enum(UserLevel), nullable=False, default=0)
     is_active = Column(Boolean, default=False)
+
+    def set_password(self, password):
+        self.password = password_context.hash(password)
+
+    def verify_password(self, password):
+        return password_context.verify(password, self.password)
+
