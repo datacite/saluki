72c410ac6a734b860c6922630f8c1f5ea20fd984
Mike Bennett <mbennett@datacite.org>
2024-01-24 19:06:13 +0000

    Add routers to main app

--- saluki/main.py
+++ saluki/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from routers.users import user_router
+from routers.datafiles import datafile_router
+from routers.permissions import permissions_router
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(datafile_router)
+app.include_router(permissions_router)
 
 
 @app.get("/")
