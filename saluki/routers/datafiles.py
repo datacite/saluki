9ba7ee743843de9c8e7e8306a1e40c135bfaf8d1
Mike Bennett <mbennett@datacite.org>
2024-01-24 14:45:11 +0000

    Add Data File CRUD functions to controller

---
+++ saluki/routers/datafiles.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from saluki.models.datafiles import create_datafile, update_datafile, remove_datafile, list_datafiles, list_datafile
+from saluki.schemas.datafiles import DataFileCreate, DataFileUpdate, DataFileInDB, DataFile
+from saluki.dependencies.database import get_database
+
+datafile_router = APIRouter(
+    prefix="/datafiles",
+    tags=["datafiles"],
+    responses={404: {"description": "Data File not found"}, 403: {"description": "Insufficient permissions"}},
+)
+
+
+@datafile_router.get("/", response_model=list[DataFile])
+def get_datafiles(skip: int = 0, limit: int = 100, db=Depends(get_database)):
+    return list_datafiles(db=db, skip=skip, limit=limit)
+
+
+@datafile_router.get("/{datafile_id}", response_model=DataFileInDB)
+def get_datafile(datafile_id: str, db=Depends(get_database)):
+    db_datafile = list_datafile(db=db, slug=datafile_id)
+    if not db_datafile:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data File not found")
+    return db_datafile
+
+
+@datafile_router.post("/", response_model=DataFile, status_code=status.HTTP_201_CREATED)
+def post_datafile(datafile: DataFileCreate, db=Depends(get_database)):
+    db_datafile = create_datafile(db=db, datafile_dict=datafile)
+    return db_datafile
+
+
+@datafile_router.put("/{datafile_id}", response_model=DataFile, status_code=status.HTTP_200_OK)
+def put_datafile(datafile_id: str, datafile: DataFileUpdate, db=Depends(get_database)):
+    db_datafile = get_datafile(db=db, slug=datafile_id)
+    if not db_datafile:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data File not found")
+    db_datafile = update_datafile(db=db, datafile=db_datafile, datafile_dict=datafile)
+    return db_datafile
+
+
+@datafile_router.delete("/{datafile_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_datafile(datafile_id: str, db=Depends(get_database)):
+    db_datafile = get_datafile(db=db, slug=datafile_id)
+    if not db_datafile:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data File not found")
+    return remove_datafile(db=db, datafile=db_datafile)
