56d0bcb9cb3d5e333278f53fb5c4f95f5502d3ea
Mike Bennett <mbennett@datacite.org>
2024-01-24 14:56:46 +0000

    Add CRUD functionality to permissions model

--- saluki/models/permissions.py
+++ saluki/models/permissions.py
@@ -1,3 +1,7 @@
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
+
+from . import DBDataFile
 from ..enums import DataFileType
 from ..dependencies.database import Base
 from sqlalchemy import Column, Integer, Enum, ForeignKey
@@ -17,3 +21,33 @@ class DBDataFileTypePermission(Base):
 
     user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
     data_file_type = Column(Enum(DataFileType), primary_key=True)
+
+
+def list_permissions_for_user(*, db: Session, user_id: int, skip: int = 0, limit: int = 100) -> list[DBDataFileTypePermission | DBDataFilePermission]:
+    return db.query(DBDataFileTypePermission, DBDataFilePermission).filter(
+        DBDataFileTypePermission.user_id == user_id,
+        DBDataFilePermission.user_id == user_id
+    ).offset(skip).limit(limit).all()
+
+
+def list_permissions_for_datafile(*, db: Session, datafile_id: int, skip: int = 0, limit: int = 100) -> list[DBDataFileTypePermission | DBDataFilePermission]:
+    return db.query(DBDataFileTypePermission, DBDataFilePermission).join(DBDataFile).filter(
+        DBDataFileTypePermission.data_file_type == DBDataFile.type,
+        DBDataFilePermission.data_file_id == datafile_id
+    ).offset(skip).limit(limit).all()
+
+
+def create_permission(*, db: Session, permission: DBDataFileTypePermission | DBDataFilePermission) -> DBDataFileTypePermission | DBDataFilePermission:
+    db.add(permission)
+    db.commit()
+    db.refresh(permission)
+    return permission
+
+
+def remove_permission(*, db: Session, permission: DBDataFileTypePermission | DBDataFilePermission) -> bool:
+    try:
+        db.delete(permission)
+        db.commit()
+        return True
+    except SQLAlchemyError as e:
+        return False
