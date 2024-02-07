e14ad700ae04b758251ef0cba9d61b97f2465bc4
Mike Bennett <mbennett@datacite.org>
2024-02-07 22:51:29 +0000

    Lint with isort and black

--- saluki/routers/datafiles.py
+++ saluki/routers/datafiles.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 
 from saluki.dependencies.database import get_database
-from saluki.dependencies.security import get_current_user, AccessLevelChecker
+from saluki.dependencies.security import AccessLevelChecker, get_current_user
 from saluki.enums import UserLevel
 from saluki.models.datafiles import (
     create_datafile,
@@ -28,7 +28,12 @@ datafile_router = APIRouter(
 
 
 @datafile_router.get("/", response_model=list[DataFile])
-def get_datafiles(skip: int = 0, limit: int = 100, db=Depends(get_database), current_user=Depends(get_current_user)):
+def get_datafiles(
+    skip: int = 0,
+    limit: int = 100,
+    db=Depends(get_database),
+    current_user=Depends(get_current_user),
+):
     if current_user.user_level >= UserLevel.editor:
         return list_datafiles(db=db, skip=skip, limit=limit)
     elif current_user.user_level == UserLevel.anonymous:
@@ -42,13 +47,18 @@ def get_datafiles(skip: int = 0, limit: int = 100, db=Depends(get_database), cur
 
 
 @datafile_router.get("/{datafile_id}", response_model=DataFile)
-def get_datafile(datafile_id: str, db=Depends(get_database), current_user=Depends(get_current_user)):
+def get_datafile(
+    datafile_id: str, db=Depends(get_database), current_user=Depends(get_current_user)
+):
     db_datafile = list_datafile(db=db, slug=datafile_id)
     if not db_datafile:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Data File not found"
         )
-    if current_user.user_level >= UserLevel.editor or db_datafile in current_user.datafiles:
+    if (
+        current_user.user_level >= UserLevel.editor
+        or db_datafile in current_user.datafiles
+    ):
         return db_datafile
     else:
         raise HTTPException(
@@ -56,14 +66,22 @@ def get_datafile(datafile_id: str, db=Depends(get_database), current_user=Depend
         )
 
 
-@datafile_router.post("/", response_model=DataFile, status_code=status.HTTP_201_CREATED, dependencies=[Depends(AccessLevelChecker(UserLevel.editor))])
+@datafile_router.post(
+    "/",
+    response_model=DataFile,
+    status_code=status.HTTP_201_CREATED,
+    dependencies=[Depends(AccessLevelChecker(UserLevel.editor))],
+)
 def post_datafile(datafile: DataFileCreate, db=Depends(get_database)):
     db_datafile = create_datafile(db=db, datafile_dict=datafile)
     return db_datafile
 
 
 @datafile_router.put(
-    "/{datafile_id}", response_model=DataFile, status_code=status.HTTP_200_OK, dependencies=[Depends(AccessLevelChecker(UserLevel.editor))]
+    "/{datafile_id}",
+    response_model=DataFile,
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(AccessLevelChecker(UserLevel.editor))],
 )
 def put_datafile(datafile_id: str, datafile: DataFileUpdate, db=Depends(get_database)):
     db_datafile = get_datafile(db=db, slug=datafile_id)
@@ -75,7 +93,11 @@ def put_datafile(datafile_id: str, datafile: DataFileUpdate, db=Depends(get_data
     return db_datafile
 
 
-@datafile_router.delete("/{datafile_id}", status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(AccessLevelChecker(UserLevel.editor))])
+@datafile_router.delete(
+    "/{datafile_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    dependencies=[Depends(AccessLevelChecker(UserLevel.editor))],
+)
 def delete_datafile(datafile_id: str, db=Depends(get_database)):
     db_datafile = get_datafile(db=db, slug=datafile_id)
     if not db_datafile:
