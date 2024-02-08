b42077096cb8ba9b18402db2ac9445d4f0989a39
Mike Bennett <mbennett@datacite.org>
2024-02-08 18:25:16 +0000

    Add basic Settings class (#21)

---
+++ saluki/config.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    sqlalchemy_database_url: str = "sqlite:///./test.db"
+    secret_key: str = "override-me-in-production"
+    jwt_expire_minutes: int = 1440
+
+    model_config = SettingsConfigDict(env_file=".env")
