83b6e0ab838f34cb6c5c9e9804a2116189f24921
Mike Bennett <mbennett@datacite.org>
2024-02-08 18:28:12 +0000

    Update security to use settings object (#21)

--- saluki/dependencies/security.py
+++ saluki/dependencies/security.py
@@ -8,18 +8,15 @@ from saluki.dependencies.database import get_database
 from saluki.enums import UserLevel
 from saluki.models import DBUser, get_user_by_email
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token", auto_error=False)
+from saluki.config import settings
 
-# TODO: Replace these with ENV variables
-SECRET_KEY = "11e3fe012c296e83632b50b920532ca42901d295043cbefcb662ac55ec5d587a"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 1440
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token", auto_error=False)
 
 
 def get_current_user(db=Depends(get_database), token=Depends(oauth2_scheme)) -> DBUser:
     if token:
         try:
-            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            payload = jwt.decode(token, settings.secret_key, algorithms=["HS256"])
             email: str = payload.get("sub")
             if email is None:
                 raise HTTPException(
@@ -68,7 +65,7 @@ class AccessLevelChecker:
 
 
 def create_access_token(user: DBUser):
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=settings.jwt_expire_minutes)
     data = {"sub": user.email, "exp": expire}
-    encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(data, settings.secret_key, algorithm=["HS256"])
     return encoded_jwt
