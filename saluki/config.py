e22512a1cf1f37b7e510285832163fb1a6fa5a89
Mike Bennett <mbennett@datacite.org>
2024-02-08 19:41:36 +0000

    Add config variables for email sending (#10)

--- saluki/config.py
+++ saluki/config.py
@@ -5,6 +5,11 @@ class Settings(BaseSettings):
     sqlalchemy_database_url: str = "sqlite:///./test.db"
     secret_key: str = "override-me-in-production"
     jwt_expire_minutes: int = 1440
+    mailgun_api_key: str = "override-me-in-production"
+    mailgun_domain: str = "mg.datacite.org"
+    mailgun_endpoint: str = "https://api.mailgun.net/v3/mg.datacite.org"
+    email_from: str = "DataCite Data Files Service"
+    email_address: str = "support@datacite.org"
 
     model_config = SettingsConfigDict(env_file=".env")
 
